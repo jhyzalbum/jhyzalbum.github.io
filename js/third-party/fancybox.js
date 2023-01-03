3bd3a7b82a333db15761e3495e22c96fb3679c0d
CH3COOH12138 <1225610429@qq.com>
2023-01-03 21:52:39 +0800

    Site updated: 2023-01-03 21:52:38

--- js/third-party/fancybox.js
+++ js/third-party/fancybox.js
@@ -3,7 +3,7 @@ document.addEventListener('page:loaded', () => {
   /**
    * Wrap images with fancybox.
    */
-  document.querySelectorAll('.post-body :not(a) > img, .post-body > img').forEach(element => {
+  document.querySelectorAll('.post-body .photos :not(a) > img, .post-body .photos > img').forEach(element => {
     const $image = $(element);
     const imageLink = $image.attr('data-src') || $image.attr('src');
     const $imageWrapLink = $image.wrap(`<a class="fancybox fancybox.image" href="${imageLink}" itemscope itemtype="http://schema.org/ImageObject" itemprop="url"></a>`).parent('a');
@@ -18,9 +18,9 @@ document.addEventListener('page:loaded', () => {
     const imageTitle = $image.attr('title') || $image.attr('alt');
     if (imageTitle) {
       // Do not append image-caption if pandoc has already created a figcaption
-      if (!$imageWrapLink.next('figcaption').length) {
-        $imageWrapLink.append(`<p class="image-caption">${imageTitle}</p>`);
-      }
+      //if (!$imageWrapLink.next('figcaption').length) {
+      //  $imageWrapLink.append(`<p class="image-caption">${imageTitle}</p>`);
+      //}
       // Make sure img title tag will show correctly in fancybox
       $imageWrapLink.attr('title', imageTitle).attr('data-caption', imageTitle);
     }
